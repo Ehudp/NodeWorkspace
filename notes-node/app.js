737d8857afe765d2429ea35deecfd17a5cf12bdf
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-10-31 21:11:14 +0200

    clean code

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,24 +1,6 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os=require('os');
 const _=require('lodash');
+
 const notes=require('./notes');
-
-//console.log(_.isString(true));
-//console.log(_.isString("true"));
-
-var filterArray=_.uniq(['test1',1,'test1',1,2,3,4]);
-console.log(filterArray);
-//var res=notes.add(9,-2);
-//console.log(res);
-
-
-//var user=os.userInfo();
-
-// fs.appendFile('greetings.txt',`hello ${user.username} you are ${notes.age} !` , (err) => {
-//     if (err) {
-//         console.log('unable to write to file');
-//     }
-// });
-
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,10 +1,5 @@
 console.log('starting notes.js');
 
-//console.log(module);
-
-module.exports.add=(num1,num2)=>{
-    return num1+num2;
-};
 
 module.exports.addNote = () => {
     console.log('add note');
