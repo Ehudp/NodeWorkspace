0dc3ece99dfdb4809ff0d55890d6e2e7e118316c
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-11-01 07:47:43 +0200

    add yargs

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,23 @@
 console.log('starting notes.js');
 
+const addNote = (title, body) => {
+    console.log('adding note ', title, body);
+};
 
-module.exports.addNote = () => {
-    console.log('add note');
-    return 'new note';
+const getAll = () => {
+    console.log('getting all notes')
+};
+const getNote = (title) => {
+    console.log('getting note', title);
+};
+const removeNote = (title) => {
+    console.log('removing note ', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
 
