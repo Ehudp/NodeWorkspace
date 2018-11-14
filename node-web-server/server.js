245feb77b54569ba94be6e58ad51ecbdd87e8113
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-11-14 06:59:13 +0200

    add hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,35 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-app.get('/', (req, res) => {
+app.set('view engine', 'hbs');
 
-    //res.send('<h1>Hello Express !</h>');
-    res.send({
-        name: 'test',
-        like: [
-            'like 1',
-            'like 2'
-        ]
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome Home'
     });
+    //res.send('<h1>Hello Express !</h>');
+    /*  res.send({
+         name: 'test',
+         like: [
+             'like 1',
+             'like 2'
+         ]
+     }); */
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server Is Up On Port 3000');
+});
