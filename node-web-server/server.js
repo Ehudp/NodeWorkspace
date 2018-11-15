4306cb45c71b9bb3c1b425d88dcce39c05e0047d
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-11-15 10:25:57 +0200

    working

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -5,6 +5,11 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
