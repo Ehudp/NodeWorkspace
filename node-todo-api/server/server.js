b06faee9926824f21350dbe2d52cc417b301aa6d
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-11-25 15:36:58 +0200

    more tests

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -17,7 +17,14 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
         res.status(400).send(e);
     });
 });
@@ -27,4 +34,3 @@ app.listen(3000, () => {
 });
 
 module.exports = { app };
-
