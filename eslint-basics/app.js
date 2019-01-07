cf05c1789376a32ce6aafb9e458fcebb2c53143a
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-01-07 12:36:36 +0200

    more jslint

--- eslint-basics/app.js
+++ eslint-basics/app.js
@@ -1,4 +1,19 @@
+//npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
 const bar = 0;
 const foo = bar + 20;
+
+const obj = {
+  name: 'hello',
+  age: 26
+};
+
+
+const work = new Promise((resolve, reject) => {
+  resolve('some data');
+});
+
+work.then((data) => {
+  console.log(data);
+});
 
 document.write(foo);
